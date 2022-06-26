93e86aa58898293ad16e0d54f71163033f0c28df
Jochen <jochen.alt@gmx.net>
2022-06-26 22:17:56 +0100

    code cleanup

--- code/controller/include/FootstepPlanner.hpp
+++ code/controller/include/FootstepPlanner.hpp
@@ -1,12 +1,8 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///
-/// \brief This is the header for FootstepPlanner class
-///
-/// \details Planner that outputs current and future locations of footsteps, the reference
-///          trajectory of the base based on the reference velocity given by the user and the current
-///          position/velocity of the base
-///
-//////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Planner that outputs current and future locations of footsteps, the reference
+ * trajectory of the base based on the reference velocity given by the user and the current
+ * position/velocity of the base
+ */
 
 #ifndef FOOTSTEPPLANNER_H_INCLUDED
 #define FOOTSTEPPLANNER_H_INCLUDED
@@ -16,7 +12,6 @@
 #include "pinocchio/multibody/model.hpp"
 #include "pinocchio/multibody/data.hpp"
 
-#include "pinocchio/math/rpy.hpp"
 #include "Gait.hpp"
 #include "Types.h"
 #include "Params.hpp"
@@ -25,157 +20,97 @@
 
 class FootstepPlanner {
  public:
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Empty constructor
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   FootstepPlanner();
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Default constructor
-  ///
-  /// \param[in] params Object that stores parameters
-  /// \param[in] gaitIn Gait object to hold the gait informations
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  void initialize(Params& params, Gait& gaitIn);
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Destructor.
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   ~FootstepPlanner() {}
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Refresh footsteps locations (computation and update of relevant matrices)
-  ///
-  ///  \param[in] refresh True if we move one step further in the gait
-  ///  \param[in] k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there
-  ///  are inter-steps)
-  ///  \param[in] q Current position vector of the flying base in horizontal frame (linear and angular stacked) +
-  ///  actuators
-  ///  \param[in] b_v Current velocity vector of the flying base in horizontal frame (linear and angular
-  ///  stacked)
-  ///  \param[in] b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular
-  ///  stacked)
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  Matrix34 updateFootsteps(bool refresh, int k, VectorN const& q, Vector6 const& b_v, Vector6 const& b_vref);
+  //	params Object that stores parameters
+  //	gaitIn Gait object to hold the gait informations
+  void initialize(Params& params, Gait& gaitIn);
+
+
+  // Refresh footsteps locations (computation and update of relevant matrices)
+  //		refresh True if we move one step further in the gait
+  //		k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there are inter-steps)
+  //		q Current position vector of the flying base in horizontal frame (linear and angular stacked) + actuators
+  //		b_v Current velocity vector of the flying base in horizontal frame (linear and angular stacked)
+  //		b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
+  Matrix34 updateFootsteps(bool refresh, int k, Vector18 const& q, Vector6 const& b_v, Vector6 const& b_vref);
 
   MatrixN12 getFootsteps();
   Matrix34 getTargetFootsteps();
 
  private:
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Compute the desired location of footsteps and update relevant matrices
-  ///
-  ///  \param[in] k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there
-  ///  are inter-steps)
-  ///  \param[in] q Current position vector of the flying base in horizontal frame (linear and
-  ///  angular stacked)
-  ///  \param[in] b_v Current velocity vector of the flying base in horizontal frame (linear and
-  ///  angular stacked)
-  ///  \param[in] b_vref Desired velocity vector of the flying base in horizontal frame (linear and
-  ///  angular stacked)
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Compute the desired location of footsteps and update relevant matrices
+  //		k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there are inter-steps)
+  //		q Current position vector of the flying base in horizontal frame (linear and angular stacked)
+  //		b_v Current velocity vector of the flying base in horizontal frame (linear and angular stacked)
+  //		b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
   Matrix34 computeTargetFootstep(int k, Vector6 const& q, Vector6 const& b_v, Vector6 const& b_vref);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Refresh feet position when entering a new contact phase
-  ///
-  ///  \param[in] q Current configuration vector
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Refresh feet position when entering a new contact phase
+  //		q Current configuration vector
   void updateNewContact(Vector18 const& q);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Compute a X by 13 matrix containing the remaining number of steps of each phase of the gait (first column)
-  ///        and the [x, y, z]^T desired position of each foot for each phase of the gait (12 other columns).
-  ///        For feet currently touching the ground the desired position is where they currently are.
-  ///
-  /// \param[in] k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there
-  ///  are inter-steps)
-  /// \param[in] b_v Current velocity vector of sthe flying base in horizontal frame (linear and angular stacked)
-  /// \param[in] b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Compute a X by 13 matrix containing the remaining number of steps of each phase of the gait (first column)
+  // and the [x, y, z]^T desired position of each foot for each phase of the gait (12 other columns).
+  // For feet currently touching the ground the desired position is where they currently are.
+  //		k Number of remaining wbc time step for the current mpc time step (wbc frequency is higher so there are inter-steps)
+  //		b_v Current velocity vector of sthe flying base in horizontal frame (linear and angular stacked)
+  //		b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
   void computeFootsteps(int k, Vector6 const& b_v, Vector6 const& b_vref);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Compute the target location on the ground of a given foot for an upcoming stance phase
-  ///
-  /// \param[in] i Considered phase (row of the gait matrix)
-  /// \param[in] j Considered foot (col of the gait matrix)
-  /// \param[in] b_v Current velocity vector of sthe flying base in horizontal frame (linear and angular stacked)
-  /// \param[in] b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
-  ///
-  /// \retval Matrix with the next footstep positions
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Compute the target location on the ground of a given foot for an upcoming stance phase
+  //		i Considered phase (row of the gait matrix)
+  //		j Considered foot (col of the gait matrix)
+  //		b_v Current velocity vector of sthe flying base in horizontal frame (linear and angular stacked)
+  //		b_vref Desired velocity vector of the flying base in horizontal frame (linear and angular stacked)
+  //		retval Matrix with the next footstep positions
   void computeNextFootstep(int i, int j, Vector6 const& b_v, Vector6 const& b_vref);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Update desired location of footsteps using information coming from the footsteps planner
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Update desired location of footsteps using information coming from the footsteps planner
   void updateTargetFootsteps();
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
-  /// \brief Transform a std::vector of N 3x4 matrices into a single Nx12 matrix
-  ///
-  /// \param[in] array The std::vector of N 3x4 matrices to transform
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Transform a std::vector of N 3x4 matrices into a single Nx12 matrix
+  //		array The std::vector of N 3x4 matrices to transform
   MatrixN12 vectorToMatrix(std::vector<Matrix34> const& array);
 
-  Params* params_;  // Params object to store parameters
-  Gait* gait_;      // Gait object to hold the gait informations
+  Params* params;  							// Params object to store parameters
+  Gait* gait;      							// Gait object to hold the gait informations
 
-  double dt;      // Time step of the contact sequence (time step of the MPC)
-  double dt_wbc;  // Time step of the whole body control
-  double h_ref;   // Reference height for the trunk
+  double dt;      							// Time step of the contact sequence (time step of the MPC)
+  double dt_wbc;  							// Time step of the whole body control
+  double h_ref;   							// Reference height for the trunk
 
   // Predefined quantities
-  double g;  // Value of the gravity acceleartion
-  double L;  // Value of the maximum allowed deviation due to leg length
+  double g;  									// Value of the gravity acceleartion
+  double L;  									// Value of the maximum allowed deviation due to leg length
 
   // Number of time steps in the prediction horizon
-  int n_steps;  // T_mpc / time step of the MPC
+  int n_steps; 	 							// T_mpc / time step of the MPC
 
   // Constant sized matrices
-  Matrix34 footsteps_under_shoulders_;  // Positions of footsteps to be "under the shoulder"
-  Matrix34 footsteps_offset_;           // Hardcoded offset to add to footsteps positions
-  Matrix34 currentFootstep_;            // Feet matrix
-  Matrix34 nextFootstep_;               // Temporary matrix to perform computations
-  Matrix34 targetFootstep_;             // In horizontal frame
-  Matrix34 o_targetFootstep_;           // targetFootstep_ in world frame
-  std::vector<Matrix34> footsteps_;     // Desired footsteps locations for each step of the horizon
+  Matrix34 footsteps_under_shoulders;  // Positions of footsteps to be "under the shoulder"
+  Matrix34 footsteps_offset;          	// Hardcoded offset to add to footsteps positions
+  Matrix34 currentFootstep;            // Feet matrix
+  Matrix34 nextFootstep;               // Temporary matrix to perform computations
+  Matrix34 targetFootstep;             // In horizontal frame
+  Matrix34 o_targetFootstep;           // targetFootstep_ in world frame
+  std::vector<Matrix34> footsteps;     // Desired footsteps locations for each step of the horizon
 
-  MatrixN Rz;      // Rotation matrix along z axis
-  VectorN dt_cum;  // Cumulated time vector
-  VectorN yaws;    // Predicted yaw variation for each cumulated time
-  VectorN dx;      // Predicted x displacement for each cumulated time
-  VectorN dy;      // Predicted y displacement for each cumulated time
+  Matrix3 Rz;      							// Rotation matrix along z axis
+  VectorN dt_cum;  							// Cumulated time vector
+  VectorN yaws;    							// Predicted yaw variation for each cumulated time
+  VectorN dx;      							// Predicted x displacement for each cumulated time
+  VectorN dy;      							// Predicted y displacement for each cumulated time
 
-  Vector3 q_dxdy;  // Temporary storage variable for offset to the future position
-  Vector3 RPY_;    // Temporary storage variable for roll pitch yaw orientation
+  Vector3 q_dxdy;  							// Temporary storage variable for offset to the future position
+  Vector3 RPY;    							// Temporary storage variable for roll pitch yaw orientation
 
-  pinocchio::Model model_;          // Pinocchio model for forward kinematics
-  pinocchio::Data data_;            // Pinocchio datas for forward kinematics
-  int foot_ids_[4] = {0, 0, 0, 0};  // Indexes of feet frames
-  Matrix34 pos_feet_;               // Estimated feet positions based on measurements
-  Vector19 q_FK_;                   // Estimated state of the base (height, roll, pitch, joints)
+  pinocchio::Model model;          		// Pinocchio model for forward kinematics
+  pinocchio::Data data;            		// Pinocchio datas for forward kinematics
+  int foot_ids_[4] = {0, 0, 0, 0};  	// Indexes of feet frames
+  Matrix34 pos_feet;               		// Estimated feet positions based on measurements
+  Vector19 q_FK;                   		// Estimated state of the base (height, roll, pitch, joints)
 };
 
 #endif  // FOOTSTEPPLANNER_H_INCLUDED
--- code/controller/src/Filter.cpp
+++ code/controller/src/Filter.cpp
@@ -1,27 +1,27 @@
 #include "Filter.hpp"
 
 Filter::Filter()
-    : b_(0.),
-      a_(Vector2::Zero()),
-      x_(Vector6::Zero()),
-      y_(VectorN::Zero(6, 1)),
-      accum_(Vector6::Zero()),
+    : b(0.),
+      a(Vector2::Zero()),
+      x(Vector6::Zero()),
+      y(VectorN::Zero(6, 1)),
+      accum(Vector6::Zero()),
       init_(false) {
 }
 
 void Filter::initialize(Params& params) {
   const double fc = 15.0;
-  b_ = (2 * M_PI * params.dt_wbc * fc) / (2 * M_PI * params.dt_wbc * fc + 1.0);
+  b = (2 * M_PI * params.dt_wbc * fc) / (2 * M_PI * params.dt_wbc * fc + 1.0);
 
-  a_ << 1.0, -(1.0 - b_);
+  a << 1.0, -(1.0 - b);
 
-  x_queue_.resize(1, Vector6::Zero());
-  y_queue_.resize(a_.rows() - 1, Vector6::Zero());
+  x_queue.resize(1, Vector6::Zero());
+  y_queue.resize(a.rows() - 1, Vector6::Zero());
 }
 
-VectorN Filter::filter(Vector6 const& x, bool check_modulo) {
+VectorN Filter::filter(Vector6 const& x_in, bool check_modulo) {
   // Retrieve measurement
-  x_ = x;
+  x = x_in;
 
   // Handle modulo for orientation
   if (check_modulo) {
@@ -29,8 +29,8 @@ VectorN Filter::filter(Vector6 const& x, bool check_modulo) {
     // Should happen sometimes for yaw but now for roll and pitch except
     // if the robot rolls over
     for (int i = 3; i < 6; i++) {
-      if (std::abs(x_(i, 0) - y_(i, 0)) > 1.5 * M_PI) {
-        handle_modulo(i, x_(i, 0) - y_(i, 0) > 0);
+      if (std::abs(x(i, 0) - y(i, 0)) > 1.5 * M_PI) {
+        handle_modulo(i, x(i, 0) - y(i, 0) > 0);
       }
     }
   }
@@ -38,35 +38,35 @@ VectorN Filter::filter(Vector6 const& x, bool check_modulo) {
   // Initialisation of the value in the queues to the first measurement
   if (!init_) {
     init_ = true;
-    std::fill(x_queue_.begin(), x_queue_.end(), x_.head(6));
-    std::fill(y_queue_.begin(), y_queue_.end(), x_.head(6));
+    std::fill(x_queue.begin(), x_queue.end(), x.head(6));
+    std::fill(y_queue.begin(), y_queue.end(), x.head(6));
   }
 
   // Store measurement in x queue
-  x_queue_.pop_back();
-  x_queue_.push_front(x_.head(6));
+  x_queue.pop_back();
+  x_queue.push_front(x.head(6));
 
   // Compute result (y/x = b/a for the transfert function)
-  accum_ = b_ * x_queue_[0];
-  for (int i = 1; i < a_.rows(); i++) {
-    accum_ -= a_[i] * y_queue_[i - 1];
+  accum = b * x_queue[0];
+  for (int i = 1; i < a.rows(); i++) {
+    accum -= a[i] * y_queue[i - 1];
   }
 
   // Store result in y queue for recursion
-  y_queue_.pop_back();
-  y_queue_.push_front(accum_ / a_[0]);
+  y_queue.pop_back();
+  y_queue.push_front(accum / a[0]);
 
   // Filtered result is stored in y_queue_.front()
   // Assigned to dynamic-sized vector for binding purpose
-  y_ = y_queue_.front();
+  y = y_queue.front();
 
-  return y_;
+  return y;
 }
 
-void Filter::handle_modulo(int a, bool dir) {
+void Filter::handle_modulo(int a_in, bool dir) {
   // Add or remove 2 PI to all elements in the queues
-  x_queue_[0](a, 0) += dir ? 2.0 * M_PI : -2.0 * M_PI;
-  for (int i = 1; i < a_.rows(); i++) {
-    (y_queue_[i - 1])(a, 0) += dir ? 2.0 * M_PI : -2.0 * M_PI;
+  x_queue[0](a_in, 0) += dir ? 2.0 * M_PI : -2.0 * M_PI;
+  for (int i = 1; i < a.rows(); i++) {
+    (y_queue[i - 1])(a_in, 0) += dir ? 2.0 * M_PI : -2.0 * M_PI;
   }
 }
