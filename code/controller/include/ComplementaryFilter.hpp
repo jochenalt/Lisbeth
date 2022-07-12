aeaedbd8851f6e84c9dd69fce9d9847e7c1b9624
Jochen <jochen.alt@gmx.net>
2022-07-12 18:46:05 +0100

    simplified

--- code/controller/include/ComplementaryFilter.hpp
+++ code/controller/include/ComplementaryFilter.hpp
@@ -5,30 +5,29 @@
 #include "Types.h"
 
 class ComplementaryFilter {
- public:
-  ComplementaryFilter();
-  ~ComplementaryFilter() {};
+	public:
+		  ComplementaryFilter();
+		  ~ComplementaryFilter() {};
 
-  // dt Time 	step of the complementary filter
-  // HighPass 	Initial value for the high pass filter
-  // LowPass 	Initial value for the low pass filter
-  ComplementaryFilter(double dt, Vector3 HighPass, Vector3 LowPass);
+		  // dt Time 	step of the complementary filter
+		  // HighPass 	Initial value for the high pass filter
+		  // LowPass 	Initial value for the low pass filter
+		  ComplementaryFilter(double dt, Vector3 HighPass, Vector3 LowPass);
 
-  //	dt Time step of the complementary filter
-  //	HighPass Initial value for the high pass filter
-  //	LowPass Initial value for the low pass filter
-  void initialize(double dt, Vector3 HighPass, Vector3 LowPass);
+		  //	dt Time step of the complementary filter
+		  //	HighPass Initial value for the high pass filter
+		  //	LowPass Initial value for the low pass filter
+		  void initialize(double dt, Vector3 HighPass, Vector3 LowPass);
 
-  // Compute the filtered output of the complementary filter
-  // 	x Quantity handled by the filter
-  //	dx Derivative of the quantity
-  //	alpha Filtering coefficient between x and dx quantities
-  Vector3 compute(Vector3 const& x, Vector3 const& dx, Vector3 const& alpha);
- private:
-  double dt;          // Time step of the complementary filter
-  Vector3 HighPass;   // Initial value for the high pass filter
-  Vector3 LowPass;    // Initial value for the low pass filter
-  Vector3 alpha;      // Filtering coefficient between x and dx quantities
+		  // Compute the filtered output of the complementary filter
+		  // 	x Quantity handled by the filter
+		  //	dx Derivative of the quantity
+		  //	alpha Filtering coefficient between x and dx quantities
+		  Vector3 compute(Vector3 const& x, Vector3 const& dx, Vector3 const& alpha);
+	private:
+		  double dt;          // Time step of the complementary filter
+		  Vector3 HighPass;   // Initial value for the high pass filter
+		  Vector3 LowPass;    // Initial value for the low pass filter
 };
 
 #endif  // COMPLEMENTARY_FILTER_H
--- code/controller/src/ComplementaryFilter.cpp
+++ code/controller/src/ComplementaryFilter.cpp
@@ -3,16 +3,14 @@
 ComplementaryFilter::ComplementaryFilter()
     : dt(0.),
       HighPass(Vector3::Zero()),
-      LowPass(Vector3::Zero()),
-      alpha(Vector3::Zero())
+      LowPass(Vector3::Zero())
 {
 }
 
 ComplementaryFilter::ComplementaryFilter(double dt_in, Vector3 HighPass_in, Vector3 LowPass_in)
     : dt(dt_in),
       HighPass(HighPass_in),
-      LowPass(LowPass_in),
-      alpha(Vector3::Zero())
+      LowPass(LowPass_in)
 {
 }
 
@@ -23,12 +21,8 @@ void ComplementaryFilter::initialize(double dt_in, Vector3 HighPass_in, Vector3 
 }
 
 Vector3 ComplementaryFilter::compute(Vector3 const& x_in, Vector3 const& dx_in, Vector3 const& alpha_in) {
-  alpha = alpha_in;
-  Vector3 x = x_in;
-  Vector3  dx = dx_in;
-
-  HighPass = alpha.cwiseProduct(HighPass + dx * dt);
-  LowPass = alpha.cwiseProduct(LowPass) + (Vector3::Ones() - alpha).cwiseProduct(x);
+  HighPass = alpha_in.cwiseProduct(HighPass + dx_in * dt);
+  LowPass = alpha_in.cwiseProduct(LowPass) + (Vector3::Ones() - alpha_in).cwiseProduct(x_in);
   Vector3 filteredX = HighPass + LowPass;
 
   return filteredX;
