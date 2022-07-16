4b78ec3740a6b9c2b44f2914ceae0560d1245ee0
jochenalt <jochen.alt@gmx.net>
2022-07-16 16:06:37 +0100

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathbase'
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
